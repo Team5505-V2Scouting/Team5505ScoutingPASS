84074c599e011b56d6ff40f3e7abc753057e2031
Team5505-V2Scouting <155908862+Team5505-V2Scouting@users.noreply.github.com>
2024-01-09 15:51:27 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "m1QYAvDqexvvbeGEPAi69N04P62DejatMXAA7dirKB6XeXFryc6vdyZ8SvE8cLGL";
 /**
  * Get list of teams in event
  *
